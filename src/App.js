8c2cf440a15263f95b0036198f9a512919ace031
eanorton <edwardnorton@icloud.com>
2018-07-13 11:10:54 -0400

    render 3d cube

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SpinningCube from './Components/SpinningCube';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+        <div className="app-intro">
+          <SpinningCube width={800} height={600}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/SpinningCube.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import * as three from 'three';
+
+class SpinningCube extends Component {
+
+  componentDidMount(){
+    const { width, height } = this.props;
+
+    const scene = new three.Scene();
+    const camera = new three.PerspectiveCamera(75, width/height, 0.1, 1000);
+    const renderer = new three.WebGLRenderer();
+
+    renderer.setSize(width, height);
+    this.refs.anchor.appendChild(renderer.domElement);
+
+    const geometry = new three.BoxGeometry(1,1,1);
+    const material = new three.MeshBasicMaterial({color:0x00ff00});
+    const cube = new three.Mesh(geometry, material);
+
+    scene.add(cube);
+    camera.position.z = 5;
+    function gameLoop(){
+      requestAnimationFrame(gameLoop);
+      cube.rotation.x += 0.1;
+      cube.rotation.y += 0.1;
+      renderer.render(scene, camera);
+    }
+    gameLoop();
+  }
+
+  render() {
+    const { width, height } = this.props;
+
+    return (
+      <div ref="anchor" style={{width, height, margin: '0 auto'}} ></div>
+    )
+  }
+}
+
+export default SpinningCube
