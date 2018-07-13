ad9191271164279d7d4a70e37dee721cdff2af4f
eanorton <edwardnorton@icloud.com>
2018-07-13 13:16:52 -0400

    updated size

--- src/Components/SpinningCube.js
+++ src/Components/SpinningCube.js
@@ -13,16 +13,18 @@ class SpinningCube extends Component {
     renderer.setSize(width, height);
     this.refs.anchor.appendChild(renderer.domElement);
 
-    const geometry = new three.BoxGeometry(1,1,1);
-    const material = new three.MeshBasicMaterial({color:0x00ff00});
+    const geometry = new three.BoxGeometry(2,2,2);
+    const material = new three.MeshBasicMaterial( {color:0x00ff00} );
     const cube = new three.Mesh(geometry, material);
 
     scene.add(cube);
+
     camera.position.z = 5;
-    function gameLoop(){
+
+    function gameLoop() {
       requestAnimationFrame(gameLoop);
-      cube.rotation.x += 0.1;
-      cube.rotation.y += 0.1;
+      cube.rotation.x += 0.03;
+      cube.rotation.y += 0.03;
       renderer.render(scene, camera);
     }
     gameLoop();
@@ -32,7 +34,7 @@ class SpinningCube extends Component {
     const { width, height } = this.props;
 
     return (
-      <div ref="anchor" style={{width, height, margin: '0 auto'}} ></div>
+      <div ref="anchor" style={ {width, height, margin: '0 auto'} }></div>
     )
   }
 }
